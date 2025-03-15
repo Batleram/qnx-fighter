4192d1483e15494b6cee60306d02ed6734a9efeb
Surya Vasudev <suryavasudev05@gmail.com>
2025-03-15 04:51:30 -0400

    push everything

--- PYGAME/game.py
+++ PYGAME/game.py
@@ -5,8 +5,7 @@ import random
 import pygame, os
 
 from scripts.utils import load_image, load_images, Animation
-from scripts.entities import Player, Enemy
-from scripts.bullet import Bullet
+from scripts.entities import Player
 from scripts.tilemap import Tilemap
 from scripts.UI import Text
 from scripts.menu import Menu
@@ -30,8 +29,8 @@ class Game:
 
         self.clock = pygame.time.Clock()
         
-        self.movement = [False, False, False, False]  # left, right, up, down
-        self.slowdown = 0 # slow down the game
+        self.movement1 = [False, False, False, False]  # left, right, up, down
+        self.movement2 = [False, False, False, False]  # left, right, up, down
         self.game_speed = 1
 
         self.assets = {
@@ -81,8 +80,9 @@ class Game:
         self.ost['deathloop'].set_volume(0.8)
 
 
-        # initalizing player
-        self.player = Player(self, (self.display.get_width()/2, self.display.get_height()/2), (42, 42))
+        # initalizing players
+        self.player1 = Player(self, (self.display.get_width()/2, self.display.get_height()/2), (42, 42))
+        self.player2 = Player(self, (self.display.get_width()/2, self.display.get_height()/2), (42, 42))
 
         # initalizing tilemap
         self.tilemap = Tilemap(self, tile_size=64)
@@ -90,12 +90,10 @@ class Game:
         self.ground = Tilemap(self, tile_size=64)
         self.ground.load('data/maps/ground.json')
 
-        self.formated_timer = '0:00'
-
         # screen shake
         self.screenshake = 0
 
-        self.dead = 0
+        self.gameover = 0
 
         self.scroll = [0, 0]
 
@@ -197,9 +195,6 @@ class Game:
             menu = Menu(self)
             menu.update()
             menu.render()
-
-            
-
             
             
             self.screen.blit(pygame.transform.scale(self.display, self.screen_size), [0,0])
@@ -229,37 +224,19 @@ class Game:
         self.playmus = True
         self.playmenumus = True
 
-        self.dead = 0
+        self.gameover = 0
 
-        self.player.pos = [self.display.get_width()/2, self.display.get_height()/2]
+        self.player1.pos = [self.display.get_width()/2, self.display.get_height()/2]
 
-        self.movement = [False, False, False, False]  # left, right, up, down
-        self.slowdown = 0 # slow down the game
+        self.movement1 = [False, False, False, False]  # left, right, up, down
+        self.movement2 = [False, False, False, False]  # left, right, up, down
         self.game_speed = 1
-
-        #Enemy spawn wait (milliseconds)
-        self.enemy_timer_reset = 1600
-        self.enemy_timer = self.enemy_timer_reset
-
-        self.enemies = []
-        self.bullets = []
-        self.sparks = []
-
-        self.deltatime = 0
-
-        self.game_timer = 30000
-
-        self.slowdown_timer_change = 3
-
-        self.has_moved = False
-
-        level_bar = Text("Time Left: " + str(self.game_timer), pos=(self.display.get_width() // 2 -30, 13))
+        
         self.ost['deathloop'].stop()
         self.ost['introloop'].stop()
         self.ost['battleloop'].stop()
         self.ost['battleloop'].play(-1)
         
-
 
         # creating an infinite game loop
         while True:
@@ -268,99 +245,35 @@ class Game:
 
             self.screenshake = max(0, self.screenshake-1) # resets screenshake value
 
-            #Count game timer down if has moved
-            if self.has_moved and not self.dead:
-                self.game_timer -= self.deltatime * (1 + self.slowdown * (self.slowdown_timer_change -1))
-
-            
-            #Count down if has moved, if time elapsed spawn enemy
-            if self.has_moved:
-                self.enemy_timer -= self.deltatime * (1 - (self.slowdown * (self.slowdown_timer_change-1)/self.slowdown_timer_change))
-            if self.enemy_timer < 0:
-                enemy_pos = [100, 100]
-                if random.randint(0, 1) == 0:
-                    enemy_pos[0] = 0 + ((self.display.get_width() + 42) * random.randint(0, 1)) - 42
-                    enemy_pos[1] = random.randint(0, self.display.get_height() + 42) - 42
-                else:
-                    enemy_pos[1] = 0 + ((self.display.get_height() + 42) * random.randint(0, 1)) - 42
-                    enemy_pos[0] = random.randint(0, self.display.get_width() + 42) - 42
-                new_enemy = Enemy(self, enemy_pos, (42, 42))
-                self.enemies.append(new_enemy)
-                #next wait (milliseconds)
-                self.enemy_timer_reset -= 100
-                if self.enemy_timer_reset < 700:
-                    self.enemy_timer_reset = 700
-                self.enemy_timer = self.enemy_timer_reset
-
             render_scroll = (0, 0)
 
             self.ground.render(self.display, offset=render_scroll)
             self.tilemap.render(self.display, offset=render_scroll)
 
-            for bullet in self.bullets.copy():
-                collided = bullet.update(self.tilemap)
-                if collided:
-                    self.bullets.remove(bullet)
-                else:
-                    if bullet.pos[0] > self.display.get_width():
-                        self.bullets.remove(bullet)
-                    if self.player.pos[1] > self.display.get_height():
-                        self.bullets.remove(bullet)
-                    if self.player.pos[0] < 0 - bullet.size[0]:
-                        self.bullets.remove(bullet)
-                    if self.player.pos[1] < 0 - bullet.size[1]:
-                        self.player.pos[1] = 0 
-                    bullet.render(self.display, offset=render_scroll)
-
-            # handle changes in game speed
-            if self.slowdown:
-                self.game_speed = 0.4
-            else:
-                self.game_speed = 1
-            
-            #update enemies
-            for enemy in self.enemies:
-                enemy.update(self.tilemap, (self.player.pos[0] - enemy.pos[0], self.player.pos[1] - enemy.pos[1]))
-                enemy.render(self.display)
-
-            if not self.dead:
-                # update player movement
-                self.player.update(self.tilemap, (self.movement[1] - self.movement[0], self.movement[3] - self.movement[2]))
-                if self.player.pos[0] > self.display.get_width() - 42:
-                    self.player.pos[0] = self.display.get_width() - 42
-                if self.player.pos[1] > self.display.get_height() - 42:
-                    self.player.pos[1] = self.display.get_height() - 42
-                if self.player.pos[0] < 0:
-                    self.player.pos[0] = 0 
-                if self.player.pos[1] < 0:
-                    self.player.pos[1] = 0 
-                self.player.render(self.display, offset=render_scroll)
-
-            for spark in self.sparks:
-                spark.update()
-                spark.render(self.display, offset=render_scroll)
-
-            #Show time left
-            min = math.floor((self.game_timer/1000/60))
-            sec = math.floor((self.game_timer/1000) % 60)
-            ms = self.game_timer - math.floor(self.game_timer/1000) * 1000
-            if sec == 0:
-                sec = '00'
-            elif sec < 10:
-                sec = '0' + str(sec)
-            if min:
-                formatted_timer = str(min) + ':' + str(sec)
-            else:
-                formatted_timer = str(sec)
-                if int(sec) <= 10:
-                    formatted_timer = formatted_timer + '.' + str(ms)
-            self.formated_timer = formatted_timer 
-            level_bar.render(self.display, 50, color=(0, 0, 0), text=formatted_timer)
-
-            if self.dead: # get hit once
-                self.dead += 1
-            if self.dead > 80:
-                self.game_over(self.formated_timer)
+            if not self.gameover:
+                # update player 1 movement
+                self.player1.update(self.tilemap, (self.movement1[1] - self.movement1[0], self.movement1[3] - self.movement1[2]))
+                if self.player1.pos[0] > self.display.get_width() - 42:
+                    self.player1.pos[0] = self.display.get_width() - 42
+                if self.player1.pos[1] > self.display.get_height() - 42:
+                    self.player1.pos[1] = self.display.get_height() - 42
+                if self.player1.pos[0] < 0:
+                    self.player1.pos[0] = 0 
+                if self.player1.pos[1] < 0:
+                    self.player1.pos[1] = 0 
+                self.player1.render(self.display, offset=render_scroll)
+                
+                # update player 2 movement
+                self.player2.update(self.tilemap, (self.movement2[1] - self.movement2[0], self.movement2[3] - self.movement2[2]))
+                if self.player2.pos[0] > self.display.get_width() - 42:
+                    self.player2.pos[0] = self.display.get_width() - 42
+                if self.player2.pos[1] > self.display.get_height() - 42:
+                    self.player2.pos[1] = self.display.get_height() - 42
+                if self.player2.pos[0] < 0:
+                    self.player2.pos[0] = 0 
+                if self.player2.pos[1] < 0:
+                    self.player2.pos[1] = 0 
+                self.player2.render(self.display, offset=render_scroll)
 
             # player cursor display bulleye
             mpos = pygame.mouse.get_pos() # gets mouse positon
@@ -373,47 +286,59 @@ class Game:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1 and not self.dead:
-                        dx = mpos[0] - self.player.rect().centerx
-                        dy = mpos[1] - self.player.rect().centery
-                        bullet_angle = math.atan2(dx, -dy) - (math.pi/2)
-                        new_bullet = Bullet(self, self.player.rect().center, 7, bullet_angle, (18, 18))
+                    if event.button == 1 and not self.gameover:
                         self.sfx['shoot'].play(0)
-                        self.bullets.append(new_bullet)
-                        self.game_timer -= 1000
                         self.screenshake = max(10, self.screenshake)
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         self.sfx['select'].play(0)
                         self.sfx['select'].play(0)
                         self.main_menu()
-                    if self.dead and event.key == pygame.K_RETURN:
+                    if self.gameover and event.key == pygame.K_RETURN:
                         self.sfx['select'].play(0)
                         self.sfx['select'].play(0)
                         self.run()
+                    
+                    # For Player 1
                     if event.key == pygame.K_a: # referencing right and left arrow keys
-                        self.movement[0] = True
+                        self.movement1[0] = True
                     elif event.key == pygame.K_d: 
-                        self.movement[1] = True
+                        self.movement1[1] = True
                     elif event.key == pygame.K_w:
-                        self.movement[2] = True
+                        self.movement1[2] = True
                     elif event.key == pygame.K_s:
-                        self.movement[3] = True
-                    self.has_moved = True
+                        self.movement1[3] = True
+                    
+                    # For Player 2
+                    if event.key == pygame.K_LEFT: # referencing right and left arrow keys
+                        self.movement2[0] = True
+                    elif event.key == pygame.K_RIGHT: 
+                        self.movement2[1] = True
+                    elif event.key == pygame.K_UP:
+                        self.movement2[2] = True
+                    elif event.key == pygame.K_DOWN:
+                        self.movement2[3] = True
+
                 if event.type == pygame.KEYUP: # when key is released
+                    # For Player 1
                     if event.key == pygame.K_a: 
-                        self.movement[0] = False
+                        self.movement1[0] = False
                     elif event.key == pygame.K_d: 
-                        self.movement[1] = False
+                        self.movement1[1] = False
                     elif event.key == pygame.K_w:
-                        self.movement[2] = False
+                        self.movement1[2] = False
                     elif event.key == pygame.K_s:
-                        self.movement[3] = False
-                
-            if self.movement[1] - self.movement[0] == 0 and self.movement[3] - self.movement[2] == 0 or self.dead:
-                self.slowdown = True
-            else:
-                self.slowdown = False
+                        self.movement1[3] = False
+                    
+                    # For Player 2
+                    if event.key == pygame.K_LEFT: # referencing right and left arrow keys
+                        self.movement2[0] = False
+                    elif event.key == pygame.K_RIGHT: 
+                        self.movement2[1] = False
+                    elif event.key == pygame.K_UP:
+                        self.movement2[2] = False
+                    elif event.key == pygame.K_DOWN:
+                        self.movement2[3] = False
             
 
             screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2)
